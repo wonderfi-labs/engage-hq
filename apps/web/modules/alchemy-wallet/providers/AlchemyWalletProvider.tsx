958ef47fb5106bf196f9818f2d4aeaa7abb405d4
parth2035 <parth.2035@gmail.com>
2025-03-31 13:27:00 -0400

    format

--- apps/web/modules/alchemy-wallet/providers/AlchemyWalletProvider.tsx
+++ apps/web/modules/alchemy-wallet/providers/AlchemyWalletProvider.tsx
@@ -1,20 +1,18 @@
 "use client";
+
 import { alchemyConfig, queryClient } from "@/modules/alchemy-wallet";
 import { AlchemyClientState } from "@account-kit/core";
 import { AlchemyAccountProvider } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
- 
-export const AlchemyWalletProvider = (
-  props: PropsWithChildren<{ initialState?: AlchemyClientState }>
-) => {
+
+export const AlchemyWalletProvider = (props: PropsWithChildren<{ initialState?: AlchemyClientState }>) => {
   return (
     <QueryClientProvider client={queryClient}>
       <AlchemyAccountProvider
         config={alchemyConfig}
         queryClient={queryClient}
-        initialState={props.initialState}
-      >
+        initialState={props.initialState}>
         {props.children}
       </AlchemyAccountProvider>
     </QueryClientProvider>
