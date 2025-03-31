26e466aecd7dc35e028218cf5ad340f48a2b5266
Parth Patel <parth.2035@gmail.com>
2025-03-31 10:40:07 -0400

    init alchemy-wallet module

--- apps/web/modules/alchemy-wallet/providers/AlchemyWalletProvider.tsx
+++ apps/web/modules/alchemy-wallet/providers/AlchemyWalletProvider.tsx
@@ -1,20 +1,39 @@
 "use client";
+<<<<<<< HEAD
 
+=======
+>>>>>>> 1f1cf956e (init alchemy-wallet module)
 import { alchemyConfig, queryClient } from "@/modules/alchemy-wallet";
 import { AlchemyClientState } from "@account-kit/core";
 import { AlchemyAccountProvider } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
+<<<<<<< HEAD
 
 export const AlchemyWalletProvider = (props: PropsWithChildren<{ initialState?: AlchemyClientState }>) => {
+=======
+ 
+export const AlchemyWalletProvider = (
+  props: PropsWithChildren<{ initialState?: AlchemyClientState }>
+) => {
+>>>>>>> 1f1cf956e (init alchemy-wallet module)
   return (
     <QueryClientProvider client={queryClient}>
       <AlchemyAccountProvider
         config={alchemyConfig}
         queryClient={queryClient}
+<<<<<<< HEAD
         initialState={props.initialState}>
+=======
+        initialState={props.initialState}
+      >
+>>>>>>> 1f1cf956e (init alchemy-wallet module)
         {props.children}
       </AlchemyAccountProvider>
     </QueryClientProvider>
   );
+<<<<<<< HEAD
 };
+=======
+};
+>>>>>>> 1f1cf956e (init alchemy-wallet module)
